22270e1821d498e063360efbd5cbc99662475967
mrxlama <89434044+mrxlama@users.noreply.github.com>
2022-02-09 19:12:26 -0800

    Update 3. CVE_Output.py

--- 3. CVE_Output.py
+++ 3. CVE_Output.py
@@ -1,16 +1,44 @@
-#!/usr/bin/env python3
+import requests
+import json
 
-import vulners
+CPE = input("Please enter CPE:\n")
+#print(type(CPE))
+
+#query = {"id":"pe:2.3:o:microsoft:windows_server_2008:r2:sp1:*:*:*:*:*:*"}
+response = requests.get("https://vuln.sentnl.io/api/cvefor/" + CPE)
+
+#cpe:2.3:a:apache:accumulo:1.5.0:*:*:*:*:*:*:*")
+#print(type(response))
+
+data = response.json()
+
+#print(data)
+#print("-" * 40 + "\n")
+#print(type(data))
+#print("-" * 40 + "\n")
+#print("DICTIONARY KEYS ARE:")
+#print(data.keys())
+
+CVE_list = [data_list['id'] for data_list in data]
+CVE = '\n'.join(CVE_list)
+
+CVSS_list = [data_list['cvss'] for data_list in data]
+
+CVSS = '\n'.join(map(str,CVSS_list))
+
+summary_list = [data_list['summary'] for data_list in data]
+summary = '\n'.join(summary_list)
+
+link_list = [data_list['references'] for data_list in data]
+#print(link_list)
+link_list2 = link_list[0]
+#print(link_list2)
+ext_link = '\n'.join(link_list2)
 
 
-def get_info():
-  vulners_api = vulners.Vulners(api_key="9SI7Z7QBF11OLHOG6HY3QM58RLJL6SP0E2V7NLT8U6IE7AI05KA9HXSUYHVN7K55")
+# get the "misc" key to get a link to remediation
 
-  heartbleed_related = vulners_api.search("heartbleed", limit=10)
-
-  print(heartbleed_related)
-
-def main():
-  get_info()
-
-main()
+print("CVE: " + CVE + "\n" +
+"CVSS: " + str(CVSS) + "\n\n" +
+"Summary: " + summary + "\n\n" + 
+"External Link(s):\n" + ext_link)
